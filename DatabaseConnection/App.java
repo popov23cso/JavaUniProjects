595fbcf5b3f40ee1af1a11cdc46ac95b4e102397
popov23cso <popov23cso@gmail.com>
2022-11-12 00:32:33 +0200

    Update App.java

--- DatabaseConnection/App.java
+++ DatabaseConnection/App.java
@@ -4,16 +4,54 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 
 public class App {
     public static void main(String[] args) throws Exception {
         Connection conn = null;
+        Scanner input = new Scanner(System.in);
+        int choice;
+        String username;
         try {
             conn = DriverManager.getConnection("jdbc:sqlite:database/users.db");
             System.out.println("Connection succesfull!");
-            insertUser(conn, "petko");
-            printUsers(conn);
+            while(true) {
+                System.out.println("1.Insert an user 2.Delete an user 3.Show all users 0.Quit program");
+                try {
+                choice = input.nextInt();
+                }
+                catch (InputMismatchException e) {
+                    System.out.println("Enter a valid choice !");
+                    input.nextLine();
+                    continue;
+                }
+                if (choice == 1) {
+                    System.out.println("Insert an user with the username:");
+                    input.nextLine();
+                    username = input.nextLine();
+                    insertUser(conn, username);
+                }
+                else if (choice == 2) {
+                    System.out.println("Delete an user with the username:");
+                    input.nextLine();
+                    username = input.nextLine();
+                    deleteUser(conn, username);
+                }
+                else if (choice == 3) {
+                    System.out.println("All of the registered users:");
+                    input.nextLine();
+                    printUsers(conn);
+                }
+                else if (choice == 0) {
+                    break;
+                }
+                else {
+                    System.out.println("Invalid output!");
+                    input.nextLine();
+                }
+            }
             
         }
         catch (SQLException e) {
@@ -21,6 +59,7 @@ public class App {
         }
         finally {
             conn.close();
+            input.close();
             System.out.println("Database closed!");
         }
 
@@ -51,5 +90,18 @@ public class App {
             System.out.println(e);
         }
 
+    }
+
+    public static void deleteUser(Connection conn, String username ) {
+        String query = "DELETE FROM users WHERE username = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, username);
+            int result = ps.executeUpdate();
+            System.out.println(result >= 1 ? "Deleted succesfully" : "No such user in database");
+        }
+        catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 }
