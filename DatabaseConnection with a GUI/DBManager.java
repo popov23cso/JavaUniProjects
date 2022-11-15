2e71b72d8478b0970cc4dc42c0c1279010a072cf
popov23cso <popov23cso@gmail.com>
2022-11-15 16:40:39 +0200

    Uploaded DB with a GUI project

---
+++ DatabaseConnection with a GUI/DBManager.java
@@ -0,0 +1,101 @@
+import java.util.HashMap;
+import java.awt.event.ActionListener;
+import java.beans.Statement;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class DBManager implements ActionListener{
+    
+    JFrame frame = new JFrame();
+    JTextField usernameField = new JTextField();
+    JButton AddBtn = new JButton("Add user");
+    JButton DeleteBtn = new JButton("Delete user");
+    JButton SaveBtn = new JButton("Save users");
+    JLabel messagelabel = new JLabel();
+    SQLiteConn conn = null;
+
+    DBManager() {
+        conn = new SQLiteConn();
+
+        messagelabel.setBounds(125, 250, 250, 35);
+        messagelabel.setFont(new Font(null, Font.ITALIC, 25));
+
+
+        AddBtn.setBounds(125, 200, 100, 25);
+        AddBtn.setFocusable(false);
+        AddBtn.addActionListener(this);
+
+        DeleteBtn.setBounds(225, 200, 100, 25);
+        DeleteBtn.setFocusable(false);
+        DeleteBtn.addActionListener(this);
+
+        SaveBtn.setBounds(10, 100, 100, 25);
+        SaveBtn.setFocusable(false);
+        SaveBtn.addActionListener(this);
+
+        usernameField.setBounds(125, 100, 200, 25);
+
+        frame.add(AddBtn);
+        frame.add(SaveBtn);
+        frame.add(usernameField);
+        frame.add(DeleteBtn);
+        frame.add(messagelabel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == DeleteBtn) {
+            String username = usernameField.getText();
+            if (username.length() <= 0) {
+                return;
+            }
+            boolean deleted = conn.deleteUser(username);
+            messagelabel.setText(deleted ? "Deleted succesfully" : "No such user");
+    
+        }
+        if(e.getSource() == AddBtn) {
+            String username = usernameField.getText();
+            if (username.length() <= 0) {
+                return;
+            }
+            boolean added = conn.insertUser(username);
+            messagelabel.setText(added ? "Inserted succesfully" : "Inserting failed");
+        }
+        if(e.getSource() == SaveBtn) {
+            try {
+                PrintWriter pw = new PrintWriter("users.txt");
+                ResultSet users = conn.getUsers();
+                pw.print("");
+                try{
+                    while(users.next()) {
+                        pw.println(users.getString("username"));
+                    }
+                }
+                catch (SQLException ex) {
+                    pw.close();
+                    return;
+                }
+                pw.close();
+            }
+            catch (FileNotFoundException ex) {
+                return;
+            }
+        }
+
+    }
+}
---
+++ DatabaseConnection with a GUI/SQLiteConn.java
@@ -0,0 +1,67 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+
+
+
+public class SQLiteConn {
+    private Connection conn = null;
+    public boolean status;
+    SQLiteConn() {
+        try {
+        conn = DriverManager.getConnection("jdbc:sqlite:database/users.db");
+        status = true;
+        }
+        catch (SQLException e) {
+            status = false;
+        }
+    }
+
+    public boolean insertUser(String username) {
+        String query = "INSERT INTO users (username) VALUES(?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, username);
+            ps.executeUpdate();
+            return true;
+        }
+        catch(SQLException e) {
+            return false;
+        }
+    }
+
+    public boolean deleteUser(String username) {
+        String query = "DELETE FROM users WHERE username = ?";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, username);
+            int result = ps.executeUpdate();
+            return (result >= 1 ? true : false);
+        }
+        catch (SQLException e) {
+            return false;
+        }
+    }
+
+
+    public ResultSet getUsers() {
+        String query = "SELECT * FROM users";
+        try {
+        Statement state = conn.createStatement();
+        ResultSet rs = state.executeQuery(query);
+        return rs;
+        }
+        catch (SQLException e)
+        {
+            return null;
+        }
+    }
+}
